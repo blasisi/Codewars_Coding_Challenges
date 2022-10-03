6b40a23e21a0013dd584d511bec634471230053c
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2022-10-03 15:00:37 +0100

    added booltoword

---
+++ 8kyu/Multiple.js
@@ -0,0 +1,14 @@
+Description:
+/*
+The code does not execute properly. Try to figure out why.
+*/
+
+
+/* My solution */
+
+    JavaScript:
+function multiply(a, b){// Function keyword and gave the function multiply and inside theres already a and b parameter
+//  console.log (a * b);
+return a*b //  return a * b;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Codewars_Coding_Challenges
+
+This Codewars_Coding_Challenges md contains codewars challenges I have completed on codewars.com using Javascripts. It also my solution helps me to improve Javascript coding skills through daily or weekly challenges..
